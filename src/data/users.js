afc8431401812e303e30d05efae8a6b599c65f93
Anna Alicia Milani <aliciamilani@users.noreply.github.com>
2020-11-16 00:21:37 -0400

    criacao arquivo de usuarios

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email: "joao@email.com",
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'
+    },
+    {
+        id: 2,
+        name: 'Mariana Aguiar',
+        email: "mariana@email.com",
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_1280.png'
+    },
+    {
+        id: 3,
+        name: 'Julia Fragoso',
+        email: "julia@email.com",
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2017/03/01/22/18/avatar-2109804_960_720.png'
+    },
+    {
+        id: 4,
+        name: 'Rafael Monteiro',
+        email: "rafael@email.com",
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png'
+    },
+];
